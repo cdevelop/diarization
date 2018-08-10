3ec3f2c0226ce15320a02dac10c657f3139c2268
lawlict <linqj6@outlook.com>
2018-08-10 11:13:38 +0800

    修改diarization.cc的VAD为energy=5

--- diarization.cc
+++ diarization.cc
@@ -326,7 +326,7 @@ int main(int argc, char *argv[])
 	mfcc_opts.frame_opts.snip_edges = false;
 
 	VadEnergyOptions vad_opts;
-	vad_opts.vad_energy_threshold = 6;  // original: 9
+	vad_opts.vad_energy_threshold = 5;  // original: 9
 	vad_opts.vad_energy_mean_scale = 0.5;
 	vad_opts.vad_frames_context = 7;
 
@@ -347,7 +347,7 @@ int main(int argc, char *argv[])
 	for(; !wav_reader.Done(); wav_reader.Next())
 	{
 		std::string name = wav_reader.Key();
-		//KALDI_LOG << "Wav File: " << name;
+		KALDI_LOG << "Wav File: " << name;
 		const WaveData &wav_data = wav_reader.Value();
 
 		Matrix<BaseFloat> mfccmat;
